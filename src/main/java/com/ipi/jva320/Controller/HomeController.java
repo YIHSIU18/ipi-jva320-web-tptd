c4634eb4357b591e80848a194419afc108c2667f
YIHSIU18 <yihsiu.lin@ipilyon.net>
2023-12-23 11:52:05 +0100

    3 commit

--- src/main/java/com/ipi/jva320/Controller/HomeController.java
+++ src/main/java/com/ipi/jva320/Controller/HomeController.java
@@ -1,12 +1,13 @@
 package com.ipi.jva320.Controller;
 
-import com.ipi.jva320.model.SalarieAideADomicile;
-import com.ipi.jva320.repository.SalarieAideADomicileRepository;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 
 @Controller
@@ -22,6 +23,30 @@ public class HomeController
         return "home";
     }
 
+    /*@GetMapping(value = "/common")
+    public String common(final ModelMap m) {
+        m.put("elts", Arrays.asList("page1", "page2"));
+        m.put("title", "Menu");
+        List<String> elements = new ArrayList<>();
+        elements.add("Élément 1");
+        elements.add("Élément 2");
+        elements.add("Élément 3");
+
+        m.put("elements", elements);
+
+        return "common";
+    }*/
+
+    @GetMapping(value ="/page1")
+    public String page1(final ModelMap m) {
+        return "fpage1";
+    }
+
+    @GetMapping(value ="/page2")
+    public String page2(final ModelMap m) {
+        m.put("elts", Arrays.asList("Page1", "Page2"));
+        return "fpage2";
+    }
 
 
 
--- src/main/java/com/ipi/jva320/Controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/Controller/SalarieController.java
@@ -10,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.server.ResponseStatusException;
 
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +25,7 @@ public class SalarieController {
 
 
    @Autowired SalarieAideADomicileService salarieAideADomicileService;
+   @Autowired SalarieAideADomicileRepository salarieAideADomicileRepository;
 
 
     @GetMapping(value = "/salaries/{id}")
@@ -29,7 +33,6 @@ public class SalarieController {
     {
         SalarieAideADomicile salarie = salarieAideADomicileService.getSalarie(id);
         model.put("salarie", salarie);
-        model.put ("actionUrl","/salaries/save"+ salarie);
 
         return "detail_Salarie";
     }
@@ -61,13 +64,56 @@ public class SalarieController {
     }
 
     @GetMapping(value = "/salaries")
-    public String listSalarie(ModelMap model)
+    public String listSalarie(@RequestParam(name = "nom", required = false) String nom,
+                              @RequestParam(name = "action", required = false) String action, ModelMap model)
     {
-        List<SalarieAideADomicile> salaries = salarieAideADomicileService.getSalaries();
-        model.put("salaries", salaries);
-        //model.put("actionUrl", "/salaries/save");
+        try
+        {
+            if ("recherche".equals(action)) {
+                // Effectuer la recherche par nom
+                List<SalarieAideADomicile> salaries = salarieAideADomicileService.getSalaries(nom);
+                model.put("salaries", salaries);
+            } else {
+                // Si aucun nom n'est fourni ou si l'action n'est pas "recherche", obtenir tous les salariés
+                List<SalarieAideADomicile> salaries = salarieAideADomicileService.getSalaries();
+                model.put("salaries", salaries);
+            }
+        }catch (EntityNotFoundException e)
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, salarieAideADomicileService.getSalaries(nom)+ " : pas trouvé");
+        }
 
         return "list";
+
+    }
+
+    @GetMapping(value = "/salaries/{id}/delete")
+    public String deleteSalarie(@PathVariable(value = "id") long id) {
+        try {
+            salarieAideADomicileService.deleteSalarieAideADomicile(id);
+        } catch (SalarieException e) {
+            // Handle exception, if needed
+            // You can also redirect to an error page
+            e.printStackTrace();
+        }
+
+        // Redirect to the list of employees after deletion
+        return "redirect:/salaries";
+    }
+
+    @PostMapping(value = "/salaries/{id}")
+    public String updateSalarie(@ModelAttribute("salarie") SalarieAideADomicile salarieUpdate, Model model) {
+        try {
+            salarieAideADomicileRepository.save(salarieUpdate);
+
+        } catch (EntityExistsException e) {
+            // Gérer l'exception selon les besoins
+            model.addAttribute("erreur", e.getMessage());
+            return "detail_Salarie";
+        }
+
+        // Redirect to the list of employees after deletion
+        return "redirect:/salaries/" + salarieUpdate.getId();
     }
 
 }
